dfeeffce4998a301c22e473779164c3f9f9c1896
SerZeMat <secemacorp@gmail.com>
2024-12-18 14:27:32 +0000

    Indentacción y corrección de estructura

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,9 +13,23 @@ class FamilyStructure:
         self.last_name = last_name
         self._next_id = 1
         self._members = [
-            {"id": self._generateId(), "first_name": "John", "last_name": self.last_name, "age": 33, "lucky_numbers": [7, 13, 22]},
-            {"id": self._generateId(), "first_name": "Jane", "last_name": self.last_name, "age": 35, "lucky_numbers": [10, 14, 3]},
-            {"id": self._generateId(), "first_name": "Jimmy", "last_name": self.last_name, "age": 5, "lucky_numbers": [1]},
+            {"id": self._generateId(),
+             "first_name": "John", 
+             "last_name": self.last_name, 
+             "age": 33, 
+             "lucky_numbers": [7, 13, 22]},
+
+            {"id": self._generateId(), 
+            "first_name": "Jane", 
+            "last_name": self.last_name, 
+            "age": 35, 
+            "lucky_numbers": [10, 14, 3]},
+
+            {"id": self._generateId(), 
+            "first_name": "Jimmy", 
+            "last_name": self.last_name, 
+            "age": 5, 
+            "lucky_numbers": [1]},
         ]
    
     # read-only: Use this method to generate random members ID's when adding members into the list
